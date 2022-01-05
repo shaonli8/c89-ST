65692546ab031f6060331246c87a7a255bb7f0c0
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2022-01-05 15:42:20 +0530

    Create CustomSidebarMenu-stylesheet.js

---
+++ CustomSidebarMenu-stylesheet.js
@@ -0,0 +1,10 @@
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    width: RFValue(140),
+    height: RFValue(140),
+    borderRadius: RFValue(70),
+    alignSelf: "center",
+    marginTop: RFValue(60),
+    resizeMode: "contain"
+  }
+});
